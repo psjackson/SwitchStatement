596665e77c84853045932dcf0a1fbfc8920adc6e
Peter Jackson <pjackson@students.mchenry.edu>
2018-01-23 21:00:22 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,49 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //create variables.
+        int number;
+        String numberString;
+        Scanner scanner = new Scanner(System.in);
+
+        //ask user for a number between 1 to 10.
+        System.out.println("Enter a whole number between 1 and 10 (inclusive): ");
+        number = scanner.nextInt();
+
+        //switch statement to get the corresponding number in italian and assign to variable.
+        switch (number){
+            case 1: numberString = "uno";
+                break;
+            case 2: numberString = "due";
+                break;
+            case 3: numberString = "tre";
+                break;
+            case 4: numberString = "quattro";
+                break;
+            case 5: numberString = "cinque";
+                break;
+            case 6: numberString = "sei";
+                break;
+            case 7: numberString = "sette";
+                break;
+            case 8: numberString = "otto";
+                break;
+            case 9: numberString = "nove";
+                break;
+            case 10: numberString = "dieci";
+                break;
+            default: numberString = "invalid";
+        }
+        //display to user the number they entered and its corresponding italian number, else display invalid input.
+        if (numberString != "invalid") {
+            System.out.println("The number " + number + " is " + numberString + " in Italian.");
+        }
+        else {
+            System.out.println("That was not a number between 1 and 10 inclusive.");
+        }
+    }
+}
